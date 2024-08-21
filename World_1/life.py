1a7a8df0d99a66020a57140a1e0fcbab05bd67ab
SteveZhang08 <SteveZhang05962022@outlook.com>
2024-08-21 16:47:07 +0800

    Update

--- World_1/life.py
+++ World_1/life.py
@@ -13,24 +13,22 @@ class life:
         '''生命繁殖'''
         shutil.copy(os.path.join(floder_path,me),os.path.join(floder_path,me+str(iterations)))
 
-    def init(n1):
+    def init(name):
         '''生命初始化'''
         try:
-            with open(os.path.join(floder_path, n1 + ".gene"), 'r') as file:  #尝试寻找基因文件
-                loaded_DNA = str(json.load(file))   #读取基因文件内容并存储在DNA中
+            loaded_DNA = get.read(name)       #读取DNA数据
         except FileNotFoundError:   #如果没有找到基因文件,则创建初始DNA
-            DNA = {
+            life_DNA = {
             "iterations": 1,
-            "name": n1,
+            "name": name,
             "food":10
             }
-            json_str = json.dumps(DNA, indent=4)    #将DNA文件存储为json并写入基因文件
+            json_str = json.dumps(life_DNA, indent=4)    #将DNA文件存储为json并写入基因文件
             with open(os.path.join(floder_path, "vita.gene"), 'w') as file:
                 file.write(json_str)
-            with open(os.path.join(floder_path, n1 + ".gene"), 'r') as file:  #重新读取基因文件
+            with open(os.path.join(floder_path, name + ".gene"), 'r') as file:  #重新读取基因文件
                 loaded_DNA = str(json.load(file))
-        DNA_list = loaded_DNA.replace("{","").replace("}","").replace(" ","").split(",")    #将基因文件转换成列表
-        return DNA_list
+        return get.DNA(loaded_DNA)                                        #解码DNA数据
 
     def eat():
         '''进食'''
@@ -47,6 +45,36 @@ class life:
         except FileExistsError:
             return False
 
+    def save(life_DNA):
+        '''存储DNA信息，需提供参数：DNA'''
+        json_str = json.dumps(life_DNA, indent=4)    #将DNA文件存储为json并写入基因文件
+        with open(os.path.join(floder_path, "vita.gene"), 'w') as file:
+            file.write(json_str)
+
+    def load(name):
+        '''读取DNA信息，需提供参数：生命名字（文件名）'''
+        loaded_DNA = get.read(name)                                         #读取DNA信息
+        return get.DNA(loaded_DNA)                                        #解码DNA数据
+
 class get:
     def name():
-        return os.path.basename(__file__).split(".")[0] 
+        return os.path.basename(__file__).split(".")[0]
+
+    def read(name):
+        '''读取DNA数据'''
+        try:
+            with open(os.path.join(floder_path, name + ".gene"), 'r') as file:  #尝试寻找基因文件
+                loaded_DNA = str(json.load(file))   #读取基因文件内容并存储在DNA中
+            return loaded_DNA
+        except:
+            return False
+
+    def DNA(loaded_DNA):
+        '''解码DNA数据'''
+        life_DNA = {}
+        DNA_list = loaded_DNA.replace("{","").replace("}","").replace(" ","").split(",")    #将基因文件转换成列表
+        for i in DNA_list:
+            life_DNA[i.split(":")[0].replace("'","")] = i.split(":")[1].replace("'","")     #将基因文件写入DNA
+        life_DNA["iterations"] = int(life_DNA["iterations"])      #修正迭代数据,从字符串修正为整数类型
+        life_DNA["food"] = int(life_DNA["food"])                  #修正能量数据,从字符串修正为整数类型
+        return life_DNA 
--- World_1/vita.py
+++ World_1/vita.py
@@ -3,24 +3,22 @@ from life import *
 name = os.path.basename(__file__).split(".")[0] #获取文件名(去除后缀)
 print(f"生命 {name} 已经启动。")
 
-DNA = {}    #创建空的DNA字典
+DNA = life.init(name)  #获取DNA信息
 
-DNA_list = life.init(name)  #获取DNA信息
-
-for i in DNA_list:
-    DNA[i.split(":")[0].replace("'","")] = i.split(":")[1].replace("'","")          #将基因文件写入DNA
-DNA["iterations"] = int(DNA["iterations"])      #修正迭代数据,从字符串修正为整数类型
-DNA["food"] = int(DNA["food"])                  #修正能量数据,从字符串修正为整数类型
 print(f"DNA数据:\n{DNA}")
 
 while alive:
+    life.load(name)
     if DNA["food"] <= 0:
         print(f"生命 {name} 死亡了。")
         alive = False
+        break
     if life.eat():
         DNA["food"] = DNA["food"] + 1
         print(f"生命 {name} 进食，当前能量值:", DNA["food"])
     else:
         print(f"生命 {name} 没找到食物，当前能量值:", DNA["food"])
+        DNA["food"] = DNA["food"] - 1
+    life.save(DNA)
 
 #life.breed(me,iterations)
