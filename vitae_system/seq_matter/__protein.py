e5bbc7b04754e1785c16c11571c221a8d72df77d
tsavpyn <tsavpyn@outlook.com>
2025-07-30 22:40:29 +0800

    对意外返回值采用泛型

--- vitae_system/seq_matter/__protein.py
+++ vitae_system/seq_matter/__protein.py
@@ -1,16 +1,18 @@
 from vitae_system.seq_matter.Enums import E_Protein, ADD_AMINO_ACIDS, INHIBIT_AMINO_ACIDS
 from vitae_system.seq_matter.Seq import ESeq
+from typing import Optional
 
 MAX_GENE_LENGTH: int = 300  # DNA的最大有效长度
 MAX_METABOLIC: float = 0.8  # 最大能量转化率
 MIN_METABOLIC: float = 0.1  # 最小能量转化率
+
 
 class Protein(ESeq[E_Protein]):
     def __init__(self, sequence: str) -> None:
         super().__init__(E_Protein)
         self.from_string(sequence)
 
-    def calc_metabolic(self) -> float:
+    def calc_metabolic(self) -> Optional[float]:
         """
         Metabolic Rate = (Num of Promoting AA − Num of Inhibitory AA) / Num of AA
         Promoting Amino Acids: L, I, V, Q, A, R, S, D
@@ -19,7 +21,7 @@ class Protein(ESeq[E_Protein]):
         Returns -1 if no sequence provided
         """
         if not self.__seq:
-            return -1
+            return None
 
         total: int = len(self.__seq)
         add_count: int = sum(1 for aa in self.__seq if aa in ADD_AMINO_ACIDS)
