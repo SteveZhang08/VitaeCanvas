8bfc35a952a979c7fa61d9649d61133487ce004d
tsavpyn <tsavpyn@outlook.com>
2025-07-31 22:38:24 +0800

    Energy移动到environment

--- vitae_system/biomolecules/__init__.py
+++ vitae_system/biomolecules/__init__.py
@@ -3,6 +3,5 @@ from .__rna import RNA
 from .__protein import Protein
 from .__codon import Codon
 from .__sugar import Sugar
-from .__energy import Energy
 from .__nadh import NADH
 from .__seq import ESeq
---
+++ vitae_system/environment/__init__.py
@@ -0,0 +1,1 @@
+from __energy import Energy
